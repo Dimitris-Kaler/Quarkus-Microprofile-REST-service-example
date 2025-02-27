78ae4f0b8f6eea8ff475ed9cf35f509d237fb25a
kalerantesd <kalerantesd@interamerican.gr>
2025-02-27 08:59:47 +0200

    add test for the first get request

--- src/test/groovy/dim/kal/resource/GreetingsResourceSpec.groovy
+++ src/test/groovy/dim/kal/resource/GreetingsResourceSpec.groovy
@@ -1,15 +1,23 @@
-package dim.kal
+package dim.kal.resource
 
+import dim.kal.model.Message
+import io.quarkus.test.junit.QuarkusTest
 import spock.lang.Specification
 import io.restassured.RestAssured
-class GreetingsResourceSpec extends Specification{
+
+
+class GreetingsResourceSpec extends Specification {
+
+GreetingsResource resource = new GreetingsResource();
+
 
     def "test hello world endpoint"() {
         when:
-        def response = RestAssured.get("/")
+        def response = resource.helloWorld()
 
         then:
-        response.statusCode() == 200
-        response.body().asString().contains("Hello World")
+        response.getStatus() == 200
+        def message = response.readEntity(Message.class)
+        message.getMsg()=="Hello World"
     }
 }
